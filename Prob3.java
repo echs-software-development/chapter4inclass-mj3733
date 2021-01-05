d90266e42866659f771e97342bd6f6d0609cbeef
Michelle Jiang <mj3733@students.necsd.net>
2021-01-05 15:24:43 +0000

    did 1-4

--- Prob3.java
+++ Prob3.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 public class Prob3 {
   public static void main(String[] args) {
-    
+  Scanner scan = new Scanner(System.in);
+  System.out.println("Enter an ASCII code: "); 
+
+  int code = scan.nextInt();
+  System.out.println((char)code);
 
 
   }
--- Prob4.java
+++ Prob4.java
@@ -2,7 +2,37 @@ import java.util.Scanner;
 
 public class Prob4 {
   public static void main(String[] args) {
-
+  Scanner scan = new Scanner(System.in);
+   System.out.println("Enter 2 characters: "); 
+  
+  String s = scan.nextLine();
+  if (s.charAt(0) == 'M'){
+   System.out.println("Mathematics");
+  }
+  else if (s.charAt(0) == 'C') {
+   System.out.println("Computer Science");
+  }
+  else if (s.charAt(0) == 'I') {
+   System.out.println("Information Technology");
+  }
+  else {
+   System.out.println("You're wrong");
+  }
+ if (s.charAt(1) == '1') {
+   System.out.println("Freshman");
+  }
+ else if (s.charAt(1) == '2') {
+   System.out.println("Sophomore");
+  }
+  else if (s.charAt(1) == '3') {
+   System.out.println("Junior");
+  }
+  else if (s.charAt(1) == '4') {
+   System.out.println("Senior");
+  }   
+  else {
+   System.out.println("That is incorrect");
+  }
 
   }
 }
